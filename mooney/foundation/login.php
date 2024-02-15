4e587208685087804ef95f64cc61924a62e4f46e
Zane Mooney <89221221+zanemooney@users.noreply.github.com>
2024-02-15 12:57:53 -0600

    added root files

---
+++ mooney/foundation/login.php
@@ -0,0 +1,12 @@
+<?php
+  if($_POST["password"] == "1830")
+  {
+    include("board-members-only/board-resources.html");
+  }
+  else
+  {
+?>
+The password is incorrect. Please contact Amy Bishop at abishop3@una.edu for help.
+<?php
+  }
+?>
